970cf7c74a35d33664227b6c05bd335b8a88bf04
Matus Faro <matusfaro@users.noreply.github.com>
2022-09-27 02:55:37 +0000

    Iterate over all regions

--- LogGroup_Retention_Script.py
+++ LogGroup_Retention_Script.py
@@ -1,21 +1,18 @@
 import boto3
 
-client = boto3.client('logs')
+dryRun = False;
 
-response = client.describe_log_groups()
-
-newlist=[]
-
-for logs in response['logGroups']:
-    newlist.append(logs['logGroupName'])
-
-print("Log group name is appended in newlist as follows")
-print(newlist)
-
-for i in newlist:
-    log=client.put_retention_policy(
-        logGroupName=i,
-        retentionInDays=30
-    )
-    print(log)
-
+responseRegions = boto3.client('ec2').describe_regions()
+for region in responseRegions['Regions']:
+    regionName = region['RegionName']
+    print(" -- ", regionName)
+    client = boto3.client('logs', region_name=regionName)
+    responseLogGroups = client.describe_log_groups()
+    for logs in responseLogGroups['logGroups']:
+        logGroupName = logs['logGroupName']
+        print("   -- ", logGroupName)
+        if not dryRun:
+            client.put_retention_policy(
+	        logGroupName=logGroupName,
+    	        retentionInDays=30
+    	    )
